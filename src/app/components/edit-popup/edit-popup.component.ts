92f8bc1545680ed7736c6b26e58de6300b022b56
Vergil Fardev <vergil.fardev@gmail.com>
2024-05-08 03:30:13 +0200

    Added forms and validators

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -3,23 +3,31 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { RatingModule } from 'primeng/rating';
 import { Product } from '../../../types';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
   imports: [
-    CommonModule, 
-    DialogModule, 
-    FormsModule, 
+    CommonModule,
+    DialogModule,
+    FormsModule,
     RatingModule,
     ButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
   @Input() header!: string;
 
   @Input() display: boolean = false;
@@ -34,10 +42,33 @@ export class EditPopupComponent {
     image: '',
     price: '',
     rating: 0,
-  }
+  };
+
+  productForm = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    image: [''],
+    price: ['', [Validators.required]],
+    rating: [0],
+  });
 
   onConfirm() {
-    this.confirm.emit(this.product);
+    // Alternative way:
+    // this.confirm.emit({
+    //   name: this.productForm.value.name || '',
+    //   image: this.productForm.value.image || '',
+    //   price: this.productForm.value.price || '',
+    //   rating: this.productForm.value.rating || 0,
+    // });
+
+    const { name, image, price, rating } = this.productForm.value;
+
+    this.confirm.emit({
+      name: name || '',
+      image: image || '',
+      price: price || '',
+      rating: rating || 0,
+    });
+
     this.display = false;
     this.displayChange.emit(this.display);
   }
@@ -45,5 +76,14 @@ export class EditPopupComponent {
   onCancel() {
     this.display = false;
     this.displayChange.emit(this.display);
+  }
+
+  ngOnChanges() {
+    this.productForm.patchValue({
+      name: this.product.name,
+      image: this.product.image,
+      price: this.product.price,
+      rating: this.product.rating,
+    });
   }
 }
